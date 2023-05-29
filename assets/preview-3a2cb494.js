4c3db7973ceb290b44563d3e1c5db769b4d78822
Adonis Lee Villamor <adonisv79@gmail.com>
2023-05-29 12:26:44 +0800

    Updates

---
+++ assets/preview-3a2cb494.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-48350bfa.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-62087c32.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-62087c32.js","./iframe-48350bfa.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-63c5d83d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-3a2cb494.js.map
